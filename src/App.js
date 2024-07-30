6a676c1e9afb2b8c15a754cca32e055a252c1c1e
ryo <ta090972@icloud.com>
2024-07-30 13:13:22 +0900

    メモのCRAD機能を追加

--- src/App.js
+++ src/App.js
@@ -1,22 +1,74 @@
-import React from "react";
+import React, { useState } from "react";
+import MemoTable from "./MemoTable.js";
+import MemoDetail from "./MemoDetail.js";
 import "./App.css";
 
 function App() {
+  const [memos, setMemos] = useState([]);
+  const [selectedMemo, setSelectedMemo] = useState({});
+
+  function handleSelectMemo(id) {
+    if (selectedMemo.id === id) {
+      setSelectedMemo({});
+    } else {
+      const content = memos.find((memo) => memo.id === id).content;
+      setSelectedMemo({ id: id, content: content });
+    }
+  }
+
+  function handleEditContent(content) {
+    setSelectedMemo({
+      ...selectedMemo,
+      content: content,
+    });
+  }
+
+  function handleSaveContent(content) {
+    const newMemos = memos.map((memo) => {
+      if (memo.id === selectedMemo.id) {
+        return {
+          id: selectedMemo.id,
+          content: content,
+        };
+      } else {
+        return memo;
+      }
+    });
+
+    return setMemos(newMemos);
+  }
+
+  function handleDeleteMemo(id) {
+    const newMemos = memos.filter((memo) => memo.id !== id);
+    setMemos(newMemos);
+    setSelectedMemo({});
+  }
+
+  function handleCreateMemo() {
+    const uuid = crypto.randomUUID();
+    const newMemo = { id: uuid, content: "新規メモ" };
+    const newMemos = [...memos];
+    newMemos.push(newMemo);
+    setMemos(newMemos);
+    setSelectedMemo(newMemo);
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <MemoTable
+        memos={memos}
+        onSelect={handleSelectMemo}
+        onCreate={handleCreateMemo}
+      />
+      {selectedMemo.id && (
+        <MemoDetail
+          id={selectedMemo.id}
+          content={selectedMemo.content}
+          onChange={handleEditContent}
+          onSave={handleSaveContent}
+          onDelete={handleDeleteMemo}
+        />
+      )}
     </div>
   );
 }
