f349fe323ecc6ca22b83d46949a55ab5c6337233
ryo <ta090972@icloud.com>
2024-07-30 23:24:30 +0900

    一覧表示に使用する文字列を、メモの先頭行のみにした

--- src/MemoTable.js
+++ src/MemoTable.js
@@ -1,10 +1,23 @@
 import React from "react";
 
 export default function MemoTable({ memos, onSelect, onCreate }) {
+  function extractFirstRow(content) {
+    const indexOfFirstRow = content.indexOf("\n");
+
+    if (indexOfFirstRow >= 1) {
+      return content.substring(0, indexOfFirstRow);
+    } else if (content === "" || indexOfFirstRow === 0) {
+      return "NO TITLE";
+    } else {
+      return content;
+    }
+  }
+
   const list = memos.map((memo) => {
+    const firstRow = extractFirstRow(memo.content);
     return (
       <li key={memo.id} onClick={() => onSelect(memo.id)}>
-        {memo.content}
+        {firstRow}
       </li>
     );
   });
