ece5cd9e71f21a5a8897fdab43c9a5394f38f62f
ryo <ta090972@icloud.com>
2024-08-13 02:26:00 +0900

    「編集」ボタンを「保存」ボタンに名称変更

--- src/MemoDetail.js
+++ src/MemoDetail.js
@@ -6,7 +6,7 @@ export default function MemoDetail({ content, onChange, onSave, onDelete }) {
     <>
       <textarea value={content} onChange={(e) => onChange(e.target.value)} />
       <div>
-        <button onClick={() => onSave()}>編集</button>
+        <button onClick={() => onSave()}>保存</button>
         <button onClick={() => onDelete()}>削除</button>
       </div>
     </>
